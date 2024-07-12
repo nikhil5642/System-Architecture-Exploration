e2bea28d18684d34a036405c423075b81eebb578
nikhil5642 <nikhilagrawal5642@gmail.com>
2024-07-12 22:33:22 +0530

    Grpc message tested

--- app/src/main/java/com/appmeito/systemarchitectureexploration/networking/GrpcClient.kt
+++ app/src/main/java/com/appmeito/systemarchitectureexploration/networking/GrpcClient.kt
@@ -3,21 +3,16 @@ package com.appmeito.systemarchitectureexploration.networking
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 import myFirstGrpc.GreeterGrpc
-import myFirstGrpc.GreeterGrpc.GreeterStub
 import myFirstGrpc.MyFirstGrpc
-import java.util.logging.Logger
 
 
 class GrpcClient(private val channel: ManagedChannel) {
 
     private val stub: GreeterGrpc.GreeterBlockingStub = GreeterGrpc.newBlockingStub(channel)
-    private val logger: Logger = Logger.getLogger(GrpcClient::class.java.name)
 
     fun sayHello(name: String): String {
         val request = MyFirstGrpc.HelloRequest.newBuilder().setName(name).build()
-        logger.info("Sending request: $request")
         val response = stub.sayHello(request)
-        logger.info("Received response: $response")
         return response.message
     }
     fun shutDown(){
